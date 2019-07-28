0077bb894c5a279589b807d1f57d2b6c656e2210
Dakota Rennemann <rennemannd@gmail.com>
2019-07-27 21:23:37 -0400

    Removing files from old project

--- untitled_project/Assets/Models/DELETE_ME.cs
+++
@@ -1,7 +0,0 @@
-﻿// Literally just delete this file
-// Seriously this file is only here as a placeholdler delete it
-// Stop reading and delete this already theres nothing here
-
-
-
-// ( ͡° ͜ʖ ͡°) ( ͡° ͜ʖ ͡°) ( ͡° ͜ʖ ͡°) ( ͡° ͜ʖ ͡°) ( ͡° ͜ʖ ͡°) ( ͡° ͜ʖ ͡°) 
